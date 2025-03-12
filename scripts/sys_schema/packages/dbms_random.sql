892aa71d19e157c80d5d594c9adc2c7b7cdeccaf
Talili, Dario Jr. <sts-dariojr.talili@rakuten.com>
2025-03-12 08:54:38 +0800

    Implements sys package for the following ticket:  - MDEV-34327: System package SYS.DBMS_RANDOM

---
+++ scripts/sys_schema/packages/dbms_random.sql
@@ -0,0 +1,137 @@
+DROP PACKAGE IF EXISTS dbms_random;
+
+SET @old_sql_mode = @@session.sql_mode, @@session.sql_mode = oracle;
+
+DELIMITER $$
+
+CREATE DEFINER='mariadb.sys'@'localhost' PACKAGE dbms_random
+  SQL SECURITY INVOKER
+  COMMENT 'Collection of random routines'
+  AS
+    PROCEDURE initialize (input INT)
+      SQL SECURITY INVOKER
+      COMMENT '
+              Description
+              -----------
+              Initializes the seed
+
+              Raises
+              ------
+
+              '
+    ;
+    PROCEDURE terminate
+      SQL SECURITY INVOKER
+      COMMENT '
+              Description
+              -----------
+              Terminates package
+
+              Raises
+              ------
+
+              '
+    ;
+    FUNCTION value (low DECIMAL, high DECIMAL) RETURN DECIMAL(65,38)
+      SQL SECURITY INVOKER
+      COMMENT '
+              Description
+              -----------
+              Gets a random Oracle Database number x, where x is greater than
+              or equal to a specified lower limit and less than a specified
+              higher limit
+
+              Raises
+              ------
+
+              '
+    ;
+    FUNCTION random RETURN INT
+      SQL SECURITY INVOKER
+      COMMENT '
+              Description
+              -----------
+              Generates a random number
+
+              Raises
+              ------
+
+              '
+    ;
+END
+$$
+
+CREATE DEFINER='mariadb.sys'@'localhost' PACKAGE BODY dbms_random
+  SQL SECURITY INVOKER
+  AS
+    PROCEDURE initialize (input INT)
+      SQL SECURITY INVOKER
+      COMMENT '
+          Description
+          -----------
+          Initializes the seed
+
+          Raises
+          ------
+
+      '
+    IS
+    BEGIN
+      SET SESSION rand_seed1 = input;
+    END;
+    PROCEDURE terminate
+      SQL SECURITY INVOKER
+      COMMENT '
+          Description
+          -----------
+          Terminates package
+
+          Raises
+          ------
+
+      '
+    IS
+    BEGIN
+    END;
+    FUNCTION value (low DECIMAL, high DECIMAL) RETURN DECIMAL(65,38)
+      SQL SECURITY INVOKER
+      COMMENT '
+          Description
+          -----------
+          Gets a random Oracle Database number x, where x is greater than
+          or equal to a specified lower limit and less than a specified
+          higher limit
+
+          Raises
+          ------
+
+      '
+    IS
+    BEGIN
+      IF low >= high THEN
+        SIGNAL SQLSTATE 'HY000' SET mysql_errno=3047, message_text='2nd argument must be greater than the 1st';
+      END IF;
+
+      RETURN RAND() * (CAST(high AS DECIMAL(65, 38)) - CAST(low AS DECIMAL(65, 38))) + CAST(low AS DECIMAL(65, 38));
+    END;
+    FUNCTION random RETURN INT
+      SQL SECURITY INVOKER
+      COMMENT '
+          Description
+          -----------
+          Generates a random number
+
+          Raises
+          ------
+
+      '
+    IS
+    BEGIN
+      RETURN CONVERT(DBMS_RANDOM.VALUE(-POW(2, 31), POW(2, 31) - 1), DECIMAL(65, 0));
+    END;
+END
+$$
+
+DELIMITER ;
+
+SET @@session.sql_mode = @old_sql_mode;
