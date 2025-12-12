1ee0dfb10c1e545753a7080878980599717033e0
Talili, Dario Jr. <sts-dariojr.talili@rakuten.com>
2025-12-12 16:43:22 +0800

    beautification

--- sql/sql_base.h
+++ sql/sql_base.h
@@ -548,18 +548,9 @@ inline bool open_and_lock_tables(THD *thd,
 inline bool open_and_lock_tables(THD *thd, TABLE_LIST *tables,
                                   bool derived, uint flags)
 {
-  /*char *dbmssql_query_substr= NULL;
-  char *query= NULL;*/
   Prelocking_strategy *prelocking_strategy;
-  /*if ((query= thd->query()) && is_valid_pointer2(query) &&
-      (dbmssql_query_substr= strcasestr(query, "dbms_sql"))
-      && strcasestr(dbmssql_query_substr, "execute")) {
-    static DBMS_SQL_prelocking_strategy dbms_strategy;
-    prelocking_strategy = &dbms_strategy;
-  } else {*/
-    static DML_prelocking_strategy dml_strategy;
-    prelocking_strategy = &dml_strategy;
-  //}
+  static DML_prelocking_strategy dml_strategy;
+  prelocking_strategy = &dml_strategy;
   if (thd->in_dbmssql_execute_context) {
     flags|= MYSQL_OPEN_IGNORE_FLUSH;
   }
